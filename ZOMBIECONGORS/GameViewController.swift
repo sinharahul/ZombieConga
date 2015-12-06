5266984d4000db80cf16d1d072ce7a70e5fce296
sinharahul <sinharahul@gmail.com>
2015-12-06 14:55:43 -0500

    refactored code till page 38

--- ZOMBIECONGORS/GameViewController.swift
+++ ZOMBIECONGORS/GameViewController.swift
@@ -12,9 +12,9 @@ import SpriteKit
 class GameViewController: UIViewController {
 
     override func viewDidLoad() {
-        super.viewDidLoad()
+            super.viewDidLoad()
 
-        if let scene = GameScene(fileNamed:"GameScene") {
+            let scene = GameScene(size:CGSize(width: 2048, height: 1536))
             // Configure the view.
             let skView = self.view as! SKView
             skView.showsFPS = true
@@ -27,27 +27,10 @@ class GameViewController: UIViewController {
             scene.scaleMode = .AspectFill
             
             skView.presentScene(scene)
-        }
+        
     }
-
-    override func shouldAutorotate() -> Bool {
-        return true
-    }
-
-    override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
-        if UIDevice.currentDevice().userInterfaceIdiom == .Phone {
-            return .AllButUpsideDown
-        } else {
-            return .All
-        }
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
-    }
-
     override func prefersStatusBarHidden() -> Bool {
         return true
     }
-}
+
+   }
