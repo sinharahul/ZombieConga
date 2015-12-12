43b52cc9a3285d67948e5978cd96f2475f6a72de
sinharahul <sinharahul@gmail.com>
2015-12-12 13:04:47 -0500

    Chapter 1 exercises -Adding zombie and doubling the size

--- ZOMBIECONGORS/GameScene.swift
+++ ZOMBIECONGORS/GameScene.swift
@@ -12,11 +12,16 @@ class GameScene: SKScene {
     override func didMoveToView(view: SKView) {
        backgroundColor=SKColor.whiteColor()
        let background=SKSpriteNode(imageNamed: "background1")
+       let zombie=SKSpriteNode(imageNamed: "zombie1")
         background.position=CGPoint(x: size.width/2, y: size.height/2)
+        zombie.position=CGPoint(x: 400, y: 400)
+        //Doubling the zombie size
+        zombie.size=CGSize(width: zombie.size.width * 2, height: zombie.size.height * 2)
         //background.zRotation=CGFloat(M_PI)/8
         //Prevent invisible zombies
         background.zPosition  = -1
        addChild(background)
+        addChild(zombie)
         let mysize=background.size
         print("Size:\(mysize)")
     }
